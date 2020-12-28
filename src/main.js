ecccea2b3e2fc30fadbf428d5f9480015e927e28
slavikse <gslavikse@gmail.com>
2020-12-28 23:51:25 +0300

    Добавлен логотип и янд статистика. Настройка сборщика.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
 
-Vue.config.productionTip = false;
+import store from './store';
+import router from './router';
+
+import './styles';
+import './configure';
+
+import App from './App.vue';
 
 new Vue({
-  router,
+  name: 'Root',
   store,
+  router,
   render: (h) => h(App),
 }).$mount('#app');
